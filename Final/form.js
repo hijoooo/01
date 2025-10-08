212e83792f352e0f02d530a6b2a1a7f713653995
hijoooo <platoskanetu@gmail.com>
2025-10-08 15:11:34 +0300

    Add files via upload

---
+++ Final/form.js
@@ -0,0 +1,8 @@
+const elementSend = document.getElementById("send");
+elementSend.addEventListener("click", send);
+
+function send() {
+    if (true) {
+        document.getElementById("UserEnter").submit();
+    }
+    }
